69b6003f35abd05eaeb864ad7e7da0442af05fe9
craz7 <dobrovolschi.maxim-alexandru@elev.cihcahul.md>
2026-04-10 08:43:43 +0300

    Modificat fisierul script.js in ramura feature3

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const img = document.getElementById("myImage");
+  if (img) {
+    img.addEventListener("click", function () {
+      alert("Ai dat click pe imagine!");
+    });
+  }
+});
